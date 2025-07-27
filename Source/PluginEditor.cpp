d573554422690db71e96263c73db7b16b79b118d
Denise N <denise.ndalamba@gmail.com>
2025-07-26 21:15:29 -0600

    implemented skeleton for custom sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -119,6 +119,13 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
 //==============================================================================
 SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor& p)
     : AudioProcessorEditor (&p), processorRef (p),
+peakFreqSlider(*processorRef.apvts.getParameter("Peak Freq"), "Hz"),
+peakGainSlider(*processorRef.apvts.getParameter("Peak Gain"), "dB"),
+peakQualitySlider(*processorRef.apvts.getParameter("Peak Quality"), ""),
+lowCutFreqSlider(*processorRef.apvts.getParameter("LowCut Freq"), "Hz"),
+highCutFreqSlider(*processorRef.apvts.getParameter("HighCut Freq"), "Hz"),
+lowCutSlopeSlider(*processorRef.apvts.getParameter("LowCut Slope"), "dB/Oct"),
+highCutSlopeSlider(*processorRef.apvts.getParameter("HighCut Slope"), "dB/Oct"),
 responseCurveComponent(processorRef),
 peakFreqSliderAttachment(processorRef.apvts, "Peak Freq", peakFreqSlider),
 peakGainSliderAttachment(processorRef.apvts, "Peak Gain", peakGainSlider),
