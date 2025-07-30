62fd4b39dace14b0a3f83da92058254f559d3053
Denise N <denise.ndalamba@gmail.com>
2025-07-30 14:29:09 -0400

    add slider range labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -92,6 +92,33 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
         startAng,
         endAng,
         *this);
+
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius  = sliderBounds.getWidth() * 0.5f;
+
+    g.setColour(Colour(144u, 238u, 144u)); // pastel green
+    g.setFont(getTextHeight());
+
+    auto numChoices = labels.size();
+    for (int i = 0; i < numChoices; ++i )
+    {
+        auto pos = labels[i].pos;
+        jassert(0.f <= pos);
+        jassert(pos <= 1.f);
+
+        auto ang = jmap(pos, 0.f, 1.f, startAng, endAng);
+
+        auto c = center.getPointOnCircumference(radius + getTextHeight() * 0.5f, ang);
+
+        Rectangle<float> r;
+        auto str = labels[i].label;
+        r.setSize(g.getCurrentFont().getStringWidth(str), getTextHeight());
+        r.setCentre(c);
+        r.setY(r.getY() + getTextHeight());
+
+        g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+
+    }
 }
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
@@ -282,6 +309,29 @@ highCutSlopeSliderAttachment(processorRef.apvts, "HighCut Slope", highCutSlopeSl
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+
+    peakFreqSlider.labels.add({0.f, "20Hz"});
+    peakFreqSlider.labels.add({1.f, "20kHz"});
+
+    peakGainSlider.labels.add({0.f, "-24dB"});
+    peakGainSlider.labels.add({1.f, "+24dB"});
+
+    peakQualitySlider.labels.add({0.f, "0.1"});
+    peakQualitySlider.labels.add({1.f, "10.0"});
+
+    lowCutFreqSlider.labels.add({0.f, "20Hz"});
+    lowCutFreqSlider.labels.add({1.f, "20kHz"});
+
+    highCutFreqSlider.labels.add({0.f, "20Hz"});
+    highCutFreqSlider.labels.add({1.f, "20kHz"});
+
+    lowCutSlopeSlider.labels.add({0.f, "12"});
+    lowCutSlopeSlider.labels.add({1.f, "48"});
+
+    highCutSlopeSlider.labels.add({0.f, "12"});
+    highCutSlopeSlider.labels.add({1.f, "48"});
+
+
     for ( auto* comp: getComps())
     {
         addAndMakeVisible(comp);
@@ -310,9 +360,12 @@ highCutSlopeSliderAttachment(processorRef.apvts, "HighCut Slope", highCutSlopeSl
         // Top 1/3 of display for showing freq responses
         // Bottom 2/3 is sliders
         auto bounds = getLocalBounds();
-        auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+        float hRatio = 25.f/ 100.f; //JUCE_LIVE_CONSTANT(33) / 100.f;
+        auto responseArea = bounds.removeFromTop(bounds.getHeight() * hRatio);
 
         responseCurveComponent.setBounds(responseArea);
+
+        bounds.removeFromTop(5);
 
         auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33);
         auto highCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5);
